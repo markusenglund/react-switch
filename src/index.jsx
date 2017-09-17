79114b29fcbcd102cc2a0bd76c99755120b68daa
yogaboll <markus.s.englund@gmail.com>
2017-09-17 20:19:34 +0700

    implement click change logic

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../src/styles.css';
 
-function Switch({ checked = true, onChange }) {
-  return (
-    <div className="react-switch">
-      <span
-        className="react-switch-bg"
-        style={{ backgroundColor: checked ? 'green' : 'grey' }}
-      />
-      <button
-        className="react-switch-toggle"
-        style={{
-          left: checked ? '29px' : '1px',
-        }}
-      />
-    </div>
-  );
+class Switch extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    const { checked, onChange } = this.props;
+    onChange(!checked);
+    // console.log(onChange, checked);
+  }
+
+  render() {
+    const { checked } = this.props;
+    return (
+      <div
+        className="react-switch"
+        onClick={this.handleClick}
+      >
+        <span
+          className="react-switch-bg"
+          style={{ backgroundColor: checked ? 'green' : 'grey' }}
+        />
+        <button
+          className="react-switch-toggle"
+          style={{
+            left: checked ? '29px' : '1px'
+          }}
+        />
+      </div>
+    );
+  }
 }
 Switch.propTypes = {
   checked: PropTypes.bool.isRequired,
