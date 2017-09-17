1afc566d56fe5eb06b7b105d6940fd65dc7d8e93
yogaboll <markus.s.englund@gmail.com>
2017-09-17 18:51:31 +0700

    create component structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../src/styles.css';
 
-function Switch() {
+function Switch({ checked = true, onChange }) {
   return (
-    <div>
-      <h1>
-        Switch Component!
-      </h1>
+    <div className="react-switch">
+      <span className="react-switch-bg" />
+      <button
+        className="react-switch-toggle"
+        style={{ left: checked ? '29px' : '1px' }}
+      />
     </div>
   );
 }
+Switch.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
 export default Switch;
