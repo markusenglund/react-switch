1a2bb8f41a75a732b6de1b644092828362277539
yogaboll <markus.s.englund@gmail.com>
2017-09-18 18:48:26 +0700

    implement react-draggable

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { DraggableCore } from 'react-draggable';
 import '../src/styles.css';
 
 class Switch extends Component {
@@ -8,7 +9,7 @@ class Switch extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.handleDragStart = this.handleDragStart.bind(this);
     this.handleDrag = this.handleDrag.bind(this);
-    this.handleDragEnd = this.handleDragEnd.bind(this);
+    this.handleDragStop = this.handleDragStop.bind(this);
     this.state = {
       left: props.checked ? 29 : 1
     };
@@ -25,68 +26,70 @@ class Switch extends Component {
     onChange(!checked);
   }
 
-  handleDragStart({ clientX, dataTransfer }) {
-    dataTransfer.setDragImage(document.createElement('DIV'), 0, 0);
+  handleDragStart({ clientX }) {
+    console.log('DRAGSTART, clientX: ', clientX);
     this.setState({ startX: clientX });
   }
 
   handleDrag({ clientX }) {
-    if (!clientX) {
-      return;
-    }
     const { checked } = this.props;
     const { startX } = this.state;
 
     const startLeft = checked ? 29 : 1;
     const newLeft = startLeft + clientX - startX;
     const left = Math.min(29, Math.max(1, newLeft));
-    this.setState({ left });
+    console.log('DRAG, newleft: ', newLeft, 'left: ', left);
+    this.setState({ left, isDragging: true });
   }
 
-  handleDragEnd() {
+  handleDragStop() {
     const { checked, onChange } = this.props;
-    const { left } = this.state;
+    const { left, isDragging } = this.state;
+    console.log('DRAGSTOP, checked: ', checked, 'left: ', left);
+    if (!isDragging) {
+      this.setState({ startX: null });
+      return onChange(!checked);
+    }
+
     if (checked) {
       if (left > 15) {
-        this.setState({ left: 29, startX: null });
-      } else {
-        onChange(false);
+        return this.setState({ left: 29, startX: null, isDragging: false });
       }
+      this.setState({ startX: null, isDragging: false });
+      return onChange(false);
     }
-    if (!checked) {
-      if (left < 15) {
-        this.setState({ left: 1, startX: null });
-      } else {
-        onChange(true);
-      }
+    if (left < 15) {
+      return this.setState({ left: 1, startX: null, isDragging: false });
     }
+    this.setState({ startX: null, isDragging: false });
+    return onChange(true);
   }
 
   render() {
     const { checked } = this.props;
     const { left, startX } = this.state;
     return (
-      /* eslint-disable jsx-a11y/no-static-element-interactions */
-      <div
-        className="react-switch"
-        onClick={this.handleClick}
-      >
-        {/* eslint-enable jsx-a11y/no-static-element-interactions */}
+      <div className="react-switch">
+        {/* eslint-disable jsx-a11y/no-static-element-interactions */ }
         <span
           className="react-switch-bg"
           style={{ backgroundColor: checked ? 'green' : 'grey' }}
+          onClick={this.handleClick}
         />
-        <button
-          className="react-switch-toggle"
-          style={{
-            left,
-            transition: startX ? null : 'left 0.2s ease-out'
-          }}
-          draggable
-          onDragStart={this.handleDragStart}
+        {/* eslint-enable jsx-a11y/no-static-element-interactions */}
+        <DraggableCore
+          onStart={this.handleDragStart}
           onDrag={this.handleDrag}
-          onDragEnd={this.handleDragEnd}
-        />
+          onStop={this.handleDragStop}
+        >
+          <button
+            className="react-switch-toggle"
+            style={{
+              left,
+              transition: startX ? null : 'left 0.2s ease-out'
+            }}
+          />
+        </DraggableCore>
       </div>
     );
   }
