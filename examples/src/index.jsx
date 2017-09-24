25adace2c94a1db023c53cbca3d558e78d2460cf
yogaboll <markus.s.englund@gmail.com>
2017-09-24 23:22:11 +0700

    make first example

--- examples/src/index.jsx
+++ examples/src/index.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Switch from '../../dist';
+import './styles.css';
+
+class Examples extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.setState({ checked });
+  }
+
+  render() {
+    const { checked } = this.state;
+
+    return (
+      <div id="examples">
+        <h1>React Switch</h1>
+        <div className="example">
+          <h2>Default usage:</h2>
+          <div className="switch-group">
+            <label htmlFor="normal-switch">Switch with default style</label>
+            <Switch
+              className="react-switch"
+              onChange={this.handleChange}
+              checked={checked}
+              id="normal-switch"
+            />
+          </div>
+          <p>The switch is <span>{checked ? 'on' : 'off'}</span>.</p>
+          <pre>{`
+handleChange(checked) {
+  this.setState({ checked });
+}
+          `}
+          </pre>
+          <pre>
+            {`
+<label htmlFor="normal-switch">Switch with default style</label>
+<Switch
+  onChange={this.handleChange}
+  checked={checked}
+  id="normal-switch"
+/>
+            `}
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
 
 render(
-  <div>
-    Hello world!, asdfadsf
-    <Switch
-      onChange={() => console.log("HEJ")}
-      checked={true}
-    />
-  </div>,
+  <Examples />,
   document.getElementById('app')
 );
