2df8db18c6cd16a01b5299f1a6c0a6226dc8ef45
yogaboll <markus.s.englund@gmail.com>
2017-10-01 00:50:53 +0700

    add snapshot tests

--- __tests__/Switch.test.jsx
+++ __tests__/Switch.test.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Switch from '../src';
 
+const noop = () => {};
+
 describe('Switch', () => {
-  it('sets state based on checked-prop', () => {
+  it('sets position based on checked-prop', () => {
     const wrapper = shallow(
       <Switch
-        onChange={() => {}}
+        onChange={noop}
         checked={false}
       />
     );
     expect(wrapper.state('left')).toBe(1);
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = render(<Switch onChange={noop} checked={false} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('matches more advanced snapshot', () => {
+    const wrapper = render(
+      <Switch
+        onChange={noop}
+        checked
+        disabled
+        onColor="#d1d2d3"
+        handleColor="pink"
+        height={80}
+        width={90}
+        id="test-test-123"
+        aria-label="super-secret-label"
+      />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
