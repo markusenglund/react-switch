93fbbefa8cb9ab7c21f7772e995eb8a1d7d103c1
yogaboll <markus.s.englund@gmail.com>
2017-10-04 00:35:51 +0700

    add tests for keydown

--- __tests__/Switch.test.jsx
+++ __tests__/Switch.test.jsx
@@ -30,6 +30,7 @@ describe('Props', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
+
 describe('onClick', () => {
   it('calls onChange when fg is clicked with !checked as argument', () => {
     const mockOnChange = jest.fn();
@@ -37,12 +38,37 @@ describe('onClick', () => {
     wrapper.find('.react-switch-fg').simulate('click');
     expect(mockOnChange.mock.calls[0][0]).toBe(true);
   });
+
   it('does not call onChange when disabled', () => {
     const mockOnChange = jest.fn();
     const wrapper = shallow(<Switch onChange={mockOnChange} checked={false} disabled />);
     wrapper.find('.react-switch-fg').simulate('click');
     expect(mockOnChange.mock.calls.length).toBe(0);
   });
+});
+
+describe('handle', () => {
+  it('sets a boxShadow on focus and loses it on blur', () => {
+    const wrapper = shallow(<Switch onChange={noop} checked={false} />);
+    wrapper.find('.react-switch-handle').simulate('focus');
+    expect(wrapper.find('.react-switch-handle').get(0).props.style.boxShadow).toBe('0px 0px 1px 2px #4D90FE');
+    wrapper.find('.react-switch-handle').simulate('blur');
+    expect(wrapper.find('.react-switch-handle').get(0).props.style.boxShadow).toBeNull();
+  });
+
+  it('calls onChange and prevent default on spacebar but not on any other key', () => {
+    const mockOnChange = jest.fn();
+    const mockPreventDefault = jest.fn();
+    const wrapper = shallow(<Switch onChange={mockOnChange} checked={false} />);
+    wrapper.find('.react-switch-handle').simulate('focus');
+    wrapper.find('.react-switch-handle').simulate('keydown', { keyCode: 32, preventDefault: mockPreventDefault });
+    expect(mockPreventDefault.mock.calls.length).toBe(1);
+    expect(mockOnChange.mock.calls[0][0]).toBe(true);
+    wrapper.find('.react-switch-handle').simulate('keydown', { keyCode: 13, preventDefault: mockPreventDefault });
+    expect(mockPreventDefault.mock.calls.length).toBe(1);
+    expect(mockOnChange.mock.calls.length).toBe(1);
+  });
+});
     // mockOnChange = jest.fn();
     // const mockPreventDefault = jest.fn();
     // wrapper.find('.react-switch-fg').simulate('keydown', { keyCode: 32, preventDefault: mockPreventDefault });
@@ -59,7 +85,6 @@ describe('onClick', () => {
   //   wrapper.find('.react-switch-fg').simulate('click');
   //   console.log(mockOnChange.mock.calls);
   // });
-});
 // describe('root div', () => {
 // Actually useful stuff for disabled
 // wrapper.setProps({ disabled: false });
