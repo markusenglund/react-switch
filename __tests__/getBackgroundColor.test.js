73d706217a55e8e2506907e70ca280842ae5b035
yogaboll <markus.s.englund@gmail.com>
2017-10-10 21:49:46 +0700

    complete test suide for getBackgroundColor()

--- __tests__/getBackgroundColor.test.js
+++ __tests__/getBackgroundColor.test.js
@@ -2,6 +2,15 @@ import getBackgroundColor from '../src/getBackgroundColor';
 
 describe('getBackgroundColor()', () => {
   it('returns onColor when pos === checkedPos', () => {
-    expect(getBackgroundColor(10, 10, '#aabbcc', '#ddeeff')).toBe('#ddeeff');
+    expect(getBackgroundColor(11, 11, '#aabbcc', '#ddeeff')).toBe('#ddeeff');
   });
+  it('returns offColor when pos === 1', () => {
+    expect(getBackgroundColor(1, 11, '#aabbcc', '#ddeeff')).toBe('#aabbcc');
+  });
+  it('returns the average when handle is in the middle', () => {
+    expect(getBackgroundColor(6, 11, '#fefe00', '#00000e')).toBe('#7f7f07');
+  });
+  it('works with  one 3-digit hex-color', () => {
+    expect(getBackgroundColor(6, 11, '#000', '#eeeeee')).toBe('#777777')
+  })
 });
