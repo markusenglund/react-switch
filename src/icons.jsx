9370b9e2171da1338d6ba2ee333609501a0d40f2
Markus Englund <markus.s.englund@gmail.com>
2019-01-06 12:04:06 +0100

    fix eslint config

--- src/icons.jsx
+++ src/icons.jsx
@@ -32,9 +32,7 @@ export const uncheckedIcon = (
     style={{ position: "absolute", top: 0 }}
   >
     <path
-      /* eslint-disable max-len */
       d="M9.9 2.12L7.78 0 4.95 2.828 2.12 0 0 2.12l2.83 2.83L0 7.776 2.123 9.9 4.95 7.07 7.78 9.9 9.9 7.776 7.072 4.95 9.9 2.12"
-      /* eslint-eable max-len */
       fill="#fff"
       fillRule="evenodd"
     />
