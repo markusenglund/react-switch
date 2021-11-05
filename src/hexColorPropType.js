03c697e2ce89ff27893dc98b673ee912e897d8af
Jack Stenglein <jackstenglein@utexas.edu>
2021-11-05 16:09:34 -0500

    Add support for hex colors with alpha values

--- src/hexColorPropType.js
+++ src/hexColorPropType.js
@@ -4,10 +4,10 @@ const hexColorPropType = (props, propName, componentName) => {
   if (
     typeof prop !== "string" ||
     prop[0] !== "#" ||
-    (prop.length !== 4 && prop.length !== 7)
+    (prop.length !== 4 && prop.length !== 7 && prop.length !== 9)
   ) {
     return new Error(
-      `Invalid prop '${propName}' supplied to '${componentName}'. '${propName}' has to be either a 3-digit or 6-digit hex-color string. Valid examples: '#abc', '#123456'`
+      `Invalid prop '${propName}' supplied to '${componentName}'. '${propName}' has to be either a 3-digit, 6-digit or 8-digit hex-color string. Valid examples: '#abc', '#123456', '#123456FF'`
     );
   }
   return null;
