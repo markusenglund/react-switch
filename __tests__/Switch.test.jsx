d54cb9c83159d0ab3cb0dfd7f9e1c2685e122505
smhg <sam.hauglustaine@gmail.com>
2022-04-29 15:18:29 +0200

    replace enzyme with testing-library

--- __tests__/Switch.test.jsx
+++ __tests__/Switch.test.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { shallow, render } from "enzyme";
-import toJson from "enzyme-to-json";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Switch from "../src";
 
 const noop = () => {};
 
 describe("Props", () => {
   it("matches snapshot with default props", () => {
-    const wrapper = render(<Switch checked={false} onChange={noop} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(<Switch checked={false} onChange={noop} />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it("matches snapshot with custom props", () => {
-    const wrapper = render(
+    const { container } = render(
       <Switch
         checked
         onChange={noop}
@@ -41,49 +40,56 @@ describe("Props", () => {
         aria-label="bar"
       />
     );
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
 
 describe("handle", () => {
   it("sets a boxShadow on focus and loses it on blur", () => {
-    const wrapper = shallow(<Switch onChange={noop} checked={false} />);
-    wrapper.find("input").simulate("focus");
+    const { container } = render(<Switch onChange={noop} checked={false} />);
+    fireEvent.focus(
+      container.querySelector("input")
+    );
     expect(
-      wrapper.find(".react-switch-handle").get(0).props.style.boxShadow
+      container.querySelector(".react-switch-handle").style.getPropertyValue('box-shadow')
     ).toBe("0 0 2px 3px #3bf");
-    wrapper.find("input").simulate("blur");
+    fireEvent.blur(
+      container.querySelector("input")
+    );
     expect(
-      wrapper.find(".react-switch-handle").get(0).props.style.boxShadow
-    ).toBeNull();
+      container.querySelector(".react-switch-handle").style.getPropertyValue('box-shadow')
+    ).toBe('');
   });
 
   it("doesn't call onChange on some other key", () => {
     const mockOnChange = jest.fn();
-    const wrapper = shallow(<Switch onChange={mockOnChange} checked={false} />);
-    wrapper.find("input").simulate("focus");
-    wrapper
-      .find("input")
-      .simulate("keydown", { keyCode: 14, preventDefault: noop });
+    const { container } = render(<Switch onChange={mockOnChange} checked={false} />);
+    fireEvent.focus(
+      container.querySelector("input")
+    );
+    fireEvent.keyDown(
+      container.querySelector("input"),
+      { keyCode: 14, preventDefault: noop }
+    );
     expect(mockOnChange).not.toBeCalled();
   });
 });
 
 describe("checked prop", () => {
   it("affects transform style when it changes", () => {
-    const wrapper = shallow(<Switch onChange={noop} checked={false} />);
-    wrapper.setProps({ checked: true });
+    const { container, rerender } = render(<Switch onChange={noop} checked={false} />);
+    rerender(<Switch onChange={noop} checked />);
     expect(
-      wrapper.find(".react-switch-handle").get(0).props.style.transform
+      container.querySelector(".react-switch-handle").style.getPropertyValue('transform')
     ).toBe("translateX(29px)");
-    wrapper.setProps({ checked: false });
+    rerender(<Switch onChange={noop} checked={false} />);
     expect(
-      wrapper.find(".react-switch-handle").get(0).props.style.transform
+      container.querySelector(".react-switch-handle").style.getPropertyValue('transform')
     ).toBe("translateX(1px)");
   });
 
   it("affects background when it changes", () => {
-    const wrapper = shallow(
+    const { container, rerender } = render(
       <Switch
         onChange={noop}
         checked={false}
@@ -91,13 +97,27 @@ describe("checked prop", () => {
         onColor="#ddeeff"
       />
     );
-    wrapper.setProps({ checked: true });
-    expect(wrapper.find(".react-switch-bg").get(0).props.style.background).toBe(
-      "#ddeeff"
+    rerender(
+      <Switch
+        onChange={noop}
+        checked
+        offColor="#aabbcc"
+        onColor="#ddeeff"
+      />
     );
-    wrapper.setProps({ checked: false });
-    expect(wrapper.find(".react-switch-bg").get(0).props.style.background).toBe(
-      "#aabbcc"
+    expect(container.querySelector(".react-switch-bg").style.getPropertyValue('background')).toBe(
+      "rgb(221, 238, 255)"
+    );
+    rerender(
+      <Switch
+        onChange={noop}
+        checked={false}
+        offColor="#aabbcc"
+        onColor="#ddeeff"
+      />
+    );
+    expect(container.querySelector(".react-switch-bg").style.getPropertyValue('background')).toBe(
+      "rgb(170, 187, 204)"
     );
   });
 });
