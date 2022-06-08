d6f8b56091c5647c4f1ec257e41bc4f6c50ca35f
markusenglund <markus@englund.dev>
2022-06-09 00:17:01 +0200

    Cherry pick changes from PR

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/react-switch.min.js");
+  module.exports = require("./dist/index.prod.js");
 } else {
-  module.exports = require("./dist/react-switch.dev.js");
+  module.exports = require("./dist/index.dev.js");
 }
